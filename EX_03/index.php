54d17a88e8bfbe6316279ed496450e72ebbac167
Abadal3108 <113515441+Abadal3108@users.noreply.github.com>
2022-10-04 10:41:14 +0200

    EX_03

--- EX_03/index.php
+++ EX_03/index.php
@@ -1,1 +1,10 @@
 <?php
+
+if(!$_POST){
+    header('Location: recive.php');
+}
+$operacion = $_POST['operacion'];
+for ($i = 0; $i<=$operacion;$i++){
+    echo 'Escribiendo '.$operacion.' líneas'.'<br>';
+}
+?>
---
+++ EX_03/recive.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body>
+<form action="index.php" method="POST" name="formulario">
+    <input type="number" placeholder="operacion:" name="operacion">
+
+    <input type="submit" value="Enviar">
+</form>
+</body>
+</html>
