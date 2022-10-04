d6f0ea2133284c88e8d3b3b2a74f76d05af8da45
Abadal3108 <113515441+Abadal3108@users.noreply.github.com>
2022-10-04 11:34:54 +0200

    EX_04

--- EX_04/index.php
+++ EX_04/index.php
@@ -1,1 +1,18 @@
 <?php
+
+if(!$_POST){
+    header('Location: recive.php');
+}
+echo $name1 = $_POST['nombre'].'<br>';
+echo $name = $_POST['apellido'].'<br>';
+echo $name = $_POST['contraseña'].'<br>';
+echo $name = $_POST['tipo'].'<br>';
+echo $name = $_POST['edad'].'<br>';
+echo $name = $_POST['comentarios'].'<br>';
+echo $name = $_POST['hidden'].'<br>';
+echo $name = $_POST['foto'].'<br>';
+
+
+
+
+?>
--- EX_04/recive.php
+++ EX_04/recive.php
@@ -4,7 +4,30 @@
 </head>
 <body>
 <form action="index.php" method="POST" name="formulario">
-    <input type="number" placeholder="operacion:" name="operacion">
+    <input type="text" placeholder="Nombre:" name="nombre">
+    <input type="text" placeholder="Apellido:" name="apellido">
+    <input type="password" placeholder="Contraseña:" name="contraseña">
+    </br>
+    </br>
+    <label for="Alumno">Alumno </label>
+    <input type="radio" name="tipo" value="Alumno" id="Alumno">
+    <label for="Profesor">Profesor </label>
+    <input type="radio" name="tipo" value="Profesor" id="Profesor">
+    </br>
+    </br>
+    <input type="number" placeholder="edad:" id="edad" name="edad"
+           min="1" max="100">
+    </br>
+    </br>
+    <textarea placeholder="comentarios:" id="comentarios" name="comentarios" cols="60" rows="5">
+Texto de prueba
+    </textarea>
+    </br>
+    </br>
+    <input type="file" name="foto" id="foto">
+    </br>
+    </br>
+    <input type="hidden" value="myPrueba" name="hidden">
 
     <input type="submit" value="Enviar">
 </form>
